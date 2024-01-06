0d3da63dd26aad79d7cef2291ac8f8e50d982a1f
Balavishnu <71483701+Vishnu857@users.noreply.github.com>
2024-01-06 10:25:39 +0530

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,10 +9,10 @@ const Contact = () => {
       <h4>Contacts</h4>
       <div className="contact_container">
         <p>
-          EMAIL:<span>tonymadhanmk20@gmail.com</span>
+          EMAIL:<span>vishnuchandran2532@gmail.com</span>
         </p>
         <p>
-          GITHUB:<span>@Tony20</span>
+          GITHUB:<span>@Vishnu857</span>
         </p>
         <div className="contact_icons">
           <a
